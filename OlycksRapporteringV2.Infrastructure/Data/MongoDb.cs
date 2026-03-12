652ceb3bddcad33fc2793eb0db88a554a20b9fea
Jakob Johansson <jakobjohansson02@hotmail.com>
2026-03-12 16:22:13 +0100

    Ändrade lösenord till mongodb

--- OlycksRapporteringV2.Infrastructure/Data/MongoDb.cs
+++ OlycksRapporteringV2.Infrastructure/Data/MongoDb.cs
@@ -3,13 +3,14 @@ using OlycksRapporteringV2.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using static OlycksRapporteringV2.Infrastructure.Data.Sercrets;
 
 namespace OlycksRapporteringV2.Infrastructure.Data
 {
     public class MongoDb
     {
         private static readonly MongoClient _client =
-            new MongoClient("mongodb+srv://jakobjohansson02_db_user:Admin1234@cluster0.01cdgjr.mongodb.net/?appName=Cluster0");
+            new MongoClient(Secrets.MongoConnectionString);
         private static readonly IMongoDatabase _database =
             _client.GetDatabase("IncidentReportDb");
         public static IMongoCollection<User> GetUserCollection()
